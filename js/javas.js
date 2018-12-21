4ef0e24421482bccba4d036b77b8fe40e71f4d62
Naimar1 <naiglyme1@gmail.com>
2018-12-21 17:49:46 +0200

    Add some function

---
+++ js/javas.js
@@ -0,0 +1,32 @@
+// FRONT-END LOGIC
+
+$(document).ready(function() {
+    $('form').submit(function(event) {
+        event.preventDefault();
+
+        var chooseAnswers = [];
+
+        $('.correct').each(function () {
+            chooseAnswers .push($(this).prop('checked'));
+        })
+
+        console.log(chooseAnswers );
+
+
+        $('#result').text("Your score is "+ getScore(chooseAnswers ));
+    })
+});
+
+// BUSINESS LOGIC
+
+function getScore(chooseAnswers ) {
+    var score = 0;
+
+    chooseAnswers .forEach(function(answer) {
+        if (answer === true) {
+            score++;
+        }
+    });
+
+    return score;
+}
